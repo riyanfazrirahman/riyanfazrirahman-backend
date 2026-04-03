2d6c61480f01fed46b6127bbd25fb8c7f408dfd2
riyanfazrirahman <riyanfazrirahman@gmail.com>
2026-04-03 15:00:07 +0700

    fix server

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import allReposHandler from "./api/repos/all.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/repos/all", allReposHandler);
+
+app.listen(PORT, () => {
+    console.log(`Server jalan di http://localhost:${PORT}`);
+});
